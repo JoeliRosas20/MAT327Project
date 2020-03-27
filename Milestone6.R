4593ea9addc3b8eab05340770e84c8ab4f0ca25b
Joel Rosas <43246011+JoeliRosas20@users.noreply.github.com>
2020-03-26 20:24:31 -0400

    Add files via upload

---
+++ Milestone6.R
@@ -0,0 +1,23 @@
+mean(District10Attendance13_19_$`Total Days`)
+#708718.6
+
+median(District10Attendance13_19_$`Total Days`)
+#719984.5
+
+var(District10Attendance13_19_$`Total Days`)
+#53932127721
+
+sd(District10Attendance13_19_$`Total Days`)
+#77040.33
+
+mean(District10Attendance13_19_$Attendance)
+#91.02179
+
+median(District10Attendance13_19_$Attendance)
+#91.95
+
+var(District10Attendance13_19_$Attendance)
+#5.812636
+
+sd(District10Attendance13_19_$Attendance)
+#2.410941
