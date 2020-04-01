8728ec4e88fdaeaeae8d22a8b439194cc9f1ed56
Joel Rosas <43246011+JoeliRosas20@users.noreply.github.com>
2020-03-31 21:06:10 -0400

    Add files via upload

---
+++ Milestone7.R
@@ -0,0 +1,3 @@
+plot(District10Attendance13_19_$`Total Days`, District10Attendance13_19_$Attendance)
+plot(District10Attendance13_19_$`Total Days`, District10Attendance13_19_$Attendance, xlab = "Total Days", ylab = "Attendance %", main = "Attendance and total days")
+cor(District10Attendance13_19_$`Total Days`, District10Attendance13_19_$Attendance)
